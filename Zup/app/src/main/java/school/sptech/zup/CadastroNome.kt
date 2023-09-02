f022f2431d5331d5d9500349347cdb970cc4c489
joohzinato <joao.sobral@sptech.school>
2023-09-01 22:37:41 -0300

    inicio do cadastro

--- Zup/app/src/main/java/school/sptech/zup/CadastroNome.kt
+++ Zup/app/src/main/java/school/sptech/zup/CadastroNome.kt
@@ -3,9 +3,9 @@ package school.sptech.zup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CadastroNome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_cadastro_nome)
     }
 }
