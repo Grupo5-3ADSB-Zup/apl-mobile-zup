4bb8f7d79e31062356c0ff0bee32035ae6a02bcd
joohzinato <joao.sobral@sptech.school>
2023-09-01 23:37:27 -0300

    inicio dos ligamentos de tela

--- Zup/app/src/main/java/school/sptech/zup/Onboarding1.kt
+++ Zup/app/src/main/java/school/sptech/zup/Onboarding1.kt
@@ -1,11 +1,26 @@
 package school.sptech.zup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class Onboarding1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding1)
     }
+
+    fun pularOnboarding(view: View) {
+        val intent = Intent(this, CadastroNome::class.java)
+        startActivity(intent)
+    }
+
+    fun proximoOnboarding2(view: View) {
+        val intent = Intent(this, Onboarding2::class.java)
+        startActivity(intent)
+    }
+
+
+
 }
--- Zup/app/src/main/java/school/sptech/zup/Onboarding3.kt
+++ Zup/app/src/main/java/school/sptech/zup/Onboarding3.kt
@@ -1,11 +1,19 @@
 package school.sptech.zup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class Onboarding3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding3)
     }
+
+    fun pularOnboarding(view: View) {
+        val intent = Intent(this, CadastroNome::class.java)
+        startActivity(intent)
+    }
+
 }
