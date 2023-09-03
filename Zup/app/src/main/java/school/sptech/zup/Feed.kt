48e131ded1ddde81a485035c3e0ef32d1ce40001
MatheusSs17 <matheus.ssantos@bandtec.com.br>
2023-09-03 03:16:10 -0300

    feature: navbar navigate

---
+++ Zup/app/src/main/java/school/sptech/zup/Feed.kt
@@ -0,0 +1,14 @@
+package school.sptech.zup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import school.sptech.zup.R
+
+class Feed : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+
+    }
+}
---
+++ Zup/app/src/main/java/school/sptech/zup/SettigsActivity.kt
@@ -0,0 +1,11 @@
+package school.sptech.zup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettigsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settigs)
+    }
+}
