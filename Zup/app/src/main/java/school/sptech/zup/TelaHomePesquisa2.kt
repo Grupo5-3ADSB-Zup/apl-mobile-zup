530264e0b0ac039b40d5392069365f75ae9c9157
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-09-26 16:35:00 -0300

    Finalizando Home | Pesquisa

---
+++ Zup/app/src/main/java/school/sptech/zup/TelaHomePesquisa2.kt
@@ -0,0 +1,11 @@
+package school.sptech.zup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaHomePesquisa2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_home_pesquisa2)
+    }
+}
