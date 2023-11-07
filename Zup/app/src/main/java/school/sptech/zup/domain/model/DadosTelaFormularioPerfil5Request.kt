6b0a30ea0b5ce09f1aa61ecf45015150d3ebc43f
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-11-07 19:47:51 -0300

    Troca

--- Zup/app/src/main/java/school/sptech/zup/domain/model/DadosTelaFormularioPerfil5Request.kt
+++ Zup/app/src/main/java/school/sptech/zup/domain/model/DadosTelaFormularioPerfil5Request.kt
@@ -3,6 +3,10 @@ package school.sptech.zup.domain.model
 import java.io.Serializable
 
 data class DadosTelaFormularioPerfil5Request(
-    val radioButtonTelaFormulario4: DadosTelaFormularioPerfil4Request?,
+    val idUsuario : String,
+    val radioButtonTelaFormulario1: String?,
+    val radioButtonTelaFormulario2: String?,
+    val radioButtonTelaFormulario3: String?,
+    val radioButtonTelaFormulario4: String,
     val radioButtonTelaFormulario5: String,
 ) : Serializable
