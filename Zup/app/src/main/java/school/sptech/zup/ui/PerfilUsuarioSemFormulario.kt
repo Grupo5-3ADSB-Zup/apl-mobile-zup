17a2af60518b2cf2ec167b46542949f07763923e
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-11-20 23:21:27 -0300

    evolução

--- Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioSemFormulario.kt
+++ Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioSemFormulario.kt
@@ -3,10 +3,21 @@ package school.sptech.zup.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import school.sptech.zup.R
+import school.sptech.zup.data.model.response.LoginResponse
+import school.sptech.zup.databinding.ActivityPerfilUsuarioSemFormularioBinding
+import school.sptech.zup.databinding.ActivityTelaConfiguracao2Binding
+import school.sptech.zup.domain.model.Sessao
 
 class PerfilUsuarioSemFormulario : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityPerfilUsuarioSemFormularioBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil_usuario_sem_formulario)
+        setContentView(binding.root)
+
+        val sessao = Sessao
+        binding.nomeUsuario.text = sessao?.nome.toString()
     }
 }
