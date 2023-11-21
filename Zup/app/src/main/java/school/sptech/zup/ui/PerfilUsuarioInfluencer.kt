836bde551e2d58e1c8c4ae257d7e21a54586c478
joohzinato <joao.sobral@sptech.school>
2023-11-20 22:51:38 -0300

    ajustes

--- Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioInfluencer.kt
+++ Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioInfluencer.kt
@@ -1,12 +1,49 @@
 package school.sptech.zup.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import school.sptech.zup.R
+import school.sptech.zup.databinding.ActivityFormularioPerfil4Binding
+import school.sptech.zup.databinding.ActivityPerfilUsuarioInfluencerBinding
 
 class PerfilUsuarioInfluencer : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilUsuarioInfluencerBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil_usuario_influencer)
+        setContentView(binding.root)
+
+        val botaoNavBar = binding.navBar
+
+        val menuItemHome = botaoNavBar.menu.findItem(R.id.botao_home)
+        val menuItemPesquisar = botaoNavBar.menu.findItem(R.id.botao_search)
+        val menuItemSettings = botaoNavBar.menu.findItem(R.id.botao_settings)
+        val menuItemPerfil = botaoNavBar.menu.findItem(R.id.botao_profile)
+
+//        menuItemHome.setOnMenuItemClickListener {
+//            call
+//            true
+//        }
+
+        menuItemPesquisar.setOnMenuItemClickListener {
+            val intent = Intent(this, BuscarInfluenciadores::class.java)
+            startActivity(intent)
+            true
+        }
+
+        menuItemSettings.setOnMenuItemClickListener {
+            val intent = Intent(this, TelaConfiguracoes::class.java)
+            startActivity(intent)
+            true
+        }
+
+        menuItemPerfil.setOnMenuItemClickListener {
+            val intent = Intent(this, PerfilUsuarioSemFormulario::class.java)
+            startActivity(intent)
+            true
+        }
     }
 }
--- Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioSemFormulario.kt
+++ Zup/app/src/main/java/school/sptech/zup/ui/PerfilUsuarioSemFormulario.kt
@@ -1,12 +1,49 @@
 package school.sptech.zup.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import school.sptech.zup.R
+import school.sptech.zup.databinding.ActivityPerfilUsuarioInfluencerBinding
+import school.sptech.zup.databinding.ActivityPerfilUsuarioSemFormularioBinding
 
 class PerfilUsuarioSemFormulario : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilUsuarioSemFormularioBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil_usuario_sem_formulario)
+        setContentView(binding.root)
+
+        val botaoNavBar = binding.navBar
+
+        val menuItemHome = botaoNavBar.menu.findItem(R.id.botao_home)
+        val menuItemPesquisar = botaoNavBar.menu.findItem(R.id.botao_search)
+        val menuItemSettings = botaoNavBar.menu.findItem(R.id.botao_settings)
+        val menuItemPerfil = botaoNavBar.menu.findItem(R.id.botao_profile)
+
+//        menuItemHome.setOnMenuItemClickListener {
+//            call
+//            true
+//        }
+
+        menuItemPesquisar.setOnMenuItemClickListener {
+            val intent = Intent(this, BuscarInfluenciadores::class.java)
+            startActivity(intent)
+            true
+        }
+
+        menuItemSettings.setOnMenuItemClickListener {
+            val intent = Intent(this, TelaConfiguracoes::class.java)
+            startActivity(intent)
+            true
+        }
+
+        menuItemPerfil.setOnMenuItemClickListener {
+            val intent = Intent(this, PerfilUsuarioSemFormulario::class.java)
+            startActivity(intent)
+            true
+        }
     }
 }
