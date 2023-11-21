84341c95edd9cffdb88afaf97f2d3da1d68d6252
Matheus Sousa Santos <matheusss@gracinha.g12.br>
2023-11-21 18:06:43 -0300

    evolucao comentario

--- Zup/app/src/main/java/school/sptech/zup/presenter/feed/Feed.kt
+++ Zup/app/src/main/java/school/sptech/zup/presenter/feed/Feed.kt
@@ -1,19 +1,18 @@
 package school.sptech.zup.presenter.feed
 
 import FeedAdapter
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import retrofit2.Call
 import school.sptech.zup.R
 import school.sptech.zup.data.model.FeedResponse
 import school.sptech.zup.databinding.ActivityFeedBinding
 import school.sptech.zup.network.ServiceProvider.service
-import school.sptech.zup.ui.BuscarInfluenciadores
-import school.sptech.zup.ui.TelaConfiguracoes
 
 
 class Feed : AppCompatActivity() {
@@ -27,6 +26,9 @@ class Feed : AppCompatActivity() {
     private val binding by lazy {
         ActivityFeedBinding.inflate(layoutInflater)
     }
+
+    val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigation)
+    val commentSection: LinearLayout = findViewById(R.id.commentSection)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,5 +60,23 @@ class Feed : AppCompatActivity() {
                 // Lidar com falhas de rede
             }
         })
+
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.getItemId()) {
+                R.id.botao_comentario -> {
+                    // Alternar a visibilidade da área de comentários
+                    if (commentSection.getVisibility() === View.VISIBLE) {
+                        commentSection.setVisibility(View.INVISIBLE)
+                    } else {
+                        commentSection.setVisibility(View.VISIBLE)
+                        // Aqui você pode adicionar a lógica para carregar e exibir os comentários existentes.
+                    }
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            false
         }
+
+    }
 }
