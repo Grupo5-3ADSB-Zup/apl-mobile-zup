9b98c302239e9367b06cb2b11f7ecc05791db9b0
joohzinato <joao.sobral@sptech.school>
2023-11-24 22:57:49 -0300

    ajustes

---
+++ Zup/app/src/main/java/school/sptech/zup/ui/CadastroInfluencer.kt
@@ -0,0 +1,12 @@
+package school.sptech.zup.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import school.sptech.zup.R
+
+class CadastroInfluencer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_influencer)
+    }
+}
