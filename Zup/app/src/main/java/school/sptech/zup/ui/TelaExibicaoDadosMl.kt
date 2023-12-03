b009926cc4dc32c3c1463f923c785b83f0a09d38
MatheusSs17 <matheus.ssantos@bandtec.com.br>
2023-12-03 04:12:50 -0300

    comentario atualizado

--- Zup/app/src/main/java/school/sptech/zup/ui/TelaExibicaoDadosMl.kt
+++ Zup/app/src/main/java/school/sptech/zup/ui/TelaExibicaoDadosMl.kt
@@ -2,34 +2,43 @@ package school.sptech.zup.ui
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
+import android.util.Base64
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
 import com.bumptech.glide.Glide
 import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import school.sptech.zup.R
 import school.sptech.zup.data.model.CalculoPesoPorNoticiaIAResponse
+import school.sptech.zup.data.model.ComentarioResponse
 import school.sptech.zup.databinding.ActivityTelaExibicaoDadosMlBinding
+import school.sptech.zup.domain.model.ComentarioRequest
 import school.sptech.zup.domain.model.DadosEnvioTelaMlRequest
 import school.sptech.zup.domain.model.Sessao
 import school.sptech.zup.network.ServiceProvider.service
 import school.sptech.zup.presenter.feed.Feed
-import retrofit2.Callback
-import retrofit2.Response
-import school.sptech.zup.data.model.ComentarioResponse
-import school.sptech.zup.data.model.FeedResponse
-import school.sptech.zup.domain.model.ComentarioRequest
+import school.sptech.zup.presenter.list.adapter.ItemPerfilUsuarioAdapter
 
-@Suppress("DEPRECATION")
+
 class TelaExibicaoDadosMl : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityTelaExibicaoDadosMlBinding.inflate(layoutInflater)
     }
 
-    val sessao = Sessao
+    private val sessao = Sessao
+    private var comentarioResponse: List<ComentarioResponse>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,22 +46,20 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
 
         val dadosFeed = intent.getSerializableExtra("dados") as? DadosEnvioTelaMlRequest
 
-        binding.PostEmissora.text = dadosFeed?.emissora.toString()
-        binding.PostTitulo.text = dadosFeed?.titulo.toString()
+        binding.PostEmissora.text = dadosFeed?.emissora.orEmpty()
+        binding.PostTitulo.text = dadosFeed?.titulo.orEmpty()
 
         val sharedPreferences = getSharedPreferences("ZupShared", Context.MODE_PRIVATE)
-
         val valorIdUsuario = sharedPreferences.getLong("idUsuario", 0)
 
         dadosFeed?.fotoNoticia?.let {
-            Glide.with(this@TelaExibicaoDadosMl) // Use o contexto do itemView
+            Glide.with(this@TelaExibicaoDadosMl)
                 .load(it)
                 .into(binding.PostImage)
-            Log.d("FeedAdapter", "Loading image at position ${dadosFeed?.id}. Image URL: $it")
+            Log.d("FeedAdapter", "Loading image at position ${dadosFeed.id}. Image URL: $it")
         }
 
-        binding.PostDescricao.text = dadosFeed?.descricao.toString()
-
+        binding.PostDescricao.text = dadosFeed?.descricao.orEmpty()
 
         val call = service.GetPorcentagemPeso(dadosFeed?.id)
         call.enqueue(object : Callback<CalculoPesoPorNoticiaIAResponse> {
@@ -62,19 +69,22 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
                 if (response.isSuccessful) {
                     val pesos = response.body()
 
-                    binding.porcentagemPesoCompra.text = if(pesos.porcentagemPesoCompra.toString() == null) "Sem dados" else pesos.porcentagemPesoCompra.toString()
-                    binding.porcentagemPesoPensaEmCompra.text = if(pesos.porcentagemPesoPensaEmCompra.toString() == null) "Sem dados" else pesos.porcentagemPesoPensaEmCompra.toString()
-                    binding.porcentagemPesoNeutro.text = if(pesos.porcentagemPesoNeutro.toString() == null) "Sem dados" else pesos.porcentagemPesoNeutro.toString()
-                    binding.porcentagemPesoPenseEmVender.text = if(pesos.porcentagemPesoPenseEmVender.toString() == null) "Sem dados" else pesos.porcentagemPesoPenseEmVender.toString()
-                    binding.porcentagemPesoVenda.text = if(pesos.porcentagemPesoVenda.toString() == null) "Sem dados" else pesos.porcentagemPesoVenda.toString()
+                    with(binding) {
+                        porcentagemPesoCompra.text = pesos?.porcentagemPesoCompra?.toString() ?: "Sem dados"
+                        porcentagemPesoPensaEmCompra.text = pesos?.porcentagemPesoPensaEmCompra?.toString() ?: "Sem dados"
+                        porcentagemPesoNeutro.text = pesos?.porcentagemPesoNeutro?.toString() ?: "Sem dados"
+                        porcentagemPesoPenseEmVender.text = pesos?.porcentagemPesoPenseEmVender?.toString() ?: "Sem dados"
+                        porcentagemPesoVenda.text = pesos?.porcentagemPesoVenda?.toString() ?: "Sem dados"
+                    }
 
                 } else {
                     mostrarErroMensagem("Sem Comentários para essa notícia")
-                    //binding.divDadosML.visibility = View.GONE
+                    binding.divDadosML.visibility = View.GONE
                 }
             }
+
             override fun onFailure(call: Call<CalculoPesoPorNoticiaIAResponse>?, t: Throwable?) {
-                // Lidar com falhas de rede
+
             }
         })
 
@@ -88,11 +98,9 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
                     val comentarioResponse = response.body()
 
                     if (comentarioResponse != null) {
-
-                        binding.commentList.text = comentarioResponse.get(0).descricao
-
+                        exibirComentarios(comentarioResponse)
                     } else {
-                        mostrarErroMensagem("Credenciais inválidas")
+                        mostrarErroMensagem("Sem Comentários para essa notícia")
                     }
                 } else {
                     mostrarErroMensagem("Erro na solicitação")
@@ -103,8 +111,8 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
                 mostrarErroMensagem("Erro na rede: ${t?.message}")
             }
         })
-        binding.submitCommentButton.setOnClickListener{
 
+        binding.submitCommentButton.setOnClickListener {
             val comentario = binding.commentInput.text.toString()
 
             val comentarioEnvio = ComentarioRequest(
@@ -118,14 +126,17 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
                     response: Response<ComentarioResponse>
                 ) {
                     if (response.isSuccessful) {
-                        val comentarioResponse = response.body()
+                        val novoComentario = response.body()
 
-                        if (comentarioResponse != null) {
-
-                            binding.commentList.text = comentarioResponse.descricao.toString()
-
+                        if (novoComentario != null) {
+                            // Adiciona o novo comentário à lista existente e exibe todos os comentários
+                            val comentariosExist = mutableListOf<ComentarioResponse>()
+                            comentariosExist.addAll(comentarioResponse ?: emptyList())
+                            comentariosExist.add(novoComentario)
+                            exibirComentarios(comentariosExist)
+                            binding.commentInput.text.clear() // Limpa o campo de entrada após adicionar o comentário
                         } else {
-                            mostrarErroMensagem("Credenciais inválidas")
+                            mostrarErroMensagem("Erro ao adicionar comentário")
                         }
                     } else {
                         mostrarErroMensagem("Erro na solicitação")
@@ -164,21 +175,39 @@ class TelaExibicaoDadosMl : AppCompatActivity() {
         }
 
         menuItemPerfil.setOnMenuItemClickListener {
-            val sharedPreferences = getSharedPreferences("ZupShared", Context.MODE_PRIVATE)
-
             val valorInfluencer = sharedPreferences.getBoolean("influencer", false)
 
-            if(sessao.influencer == true || valorInfluencer == true){
-                val intent = Intent(this, PerfilUsuarioInfluencer::class.java)
-                startActivity(intent)
-                true
-            }else {
-                val intent = Intent(this, PerfilUsuarioSemFormulario::class.java)
-                startActivity(intent)
-                true
+            val intent = if(sessao.influencer == true || valorInfluencer == true) {
+                Intent(this, PerfilUsuarioInfluencer::class.java)
+            } else {
+                Intent(this, PerfilUsuarioSemFormulario::class.java)
             }
+
+            startActivity(intent)
+            true
         }
     }
+
+    private fun exibirComentarios(comentarios: List<ComentarioResponse>) {
+        val comentariosOrdenados = comentarios.sortedByDescending { it.id }
+        val comentariosText = comentariosOrdenados.joinToString("\n") { comentario ->
+            "${colocarFotoUsuario(comentario.usuario.foto)} ${comentario.usuario.nome}: ${comentario.descricao}"
+        }
+        binding.commentList.text = comentariosText
+    }
+
+    private fun colocarFotoUsuario(foto: String): SpannableString {
+        val spannableString = SpannableString(" ")
+        val imageSpan = ImageSpan(getBitmapFromBase64(foto), ImageSpan.ALIGN_BASELINE)
+        spannableString.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return spannableString
+    }
+
+    private fun getBitmapFromBase64(base64: String): Bitmap {
+        val decodedBytes: ByteArray = Base64.decode(base64, Base64.DEFAULT)
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    }
+
 
     private fun mostrarErroMensagem(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
